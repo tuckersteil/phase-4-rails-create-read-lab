fa831c1b19b8bafed19ebb4dc0ef21acc94d948c
Tucker Steil <tuckersteil@gmail.com>
2023-01-04 13:28:13 -0800

    commit tree e20786789f6b9f28f18d3c95af1c8ba8fa6c345f

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,21 @@
+class PlantsController < ApplicationController
+
+    def index 
+        plants = Plant.all
+        render json: plants 
+    end 
+
+    def show
+        plant = Plant.find_by(id:params[:id]) 
+        render json: plant
+    end 
+
+    def create
+        plant = Plant.create(plant_params)
+        render json: plant, status: :created
+    end 
+
+    def plant_params
+        params.permit(:name, :image, :price)
+    end 
+end
